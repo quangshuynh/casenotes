2e87c847f1e83f79bc92220ee61128275d3df2b5
Quang <20378quang@gmail.com>
2026-08-27 14:11:20 -0400

    Initial Commit

---
+++ CaseNotes/CaseNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseNotesApp.swift
+//  CaseNotes
+//
+//  Created by q on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CaseNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseNotesTests/CaseNotesTests.swift
@@ -0,0 +1,19 @@
+//
+//  CaseNotesTests.swift
+//  CaseNotesTests
+//
+//  Created by q on 8/27/26.
+//
+
+import Testing
+@testable import CaseNotes
+
+struct CaseNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
